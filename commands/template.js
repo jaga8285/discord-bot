17e5a2e4a81f89579df1b09ccc805d958ba8984f
jaga8285 <vitamina@tangerina.org>
2021-06-16 21:10:58 +0100

    template no argument behaviour

--- commands/template.js
+++ commands/template.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const path = require('path');
 const config = require(path.join(__dirname+'/../config.json'));
 const fs = require('fs');
@@ -10,6 +11,16 @@ module.exports = {
   usage: config.prefix+ 'template https://{cred}flare.tangerina.org/lab16/src/episodes/Banana/BananaFishEp{ep}.mkv',
   alias: ['t'],
   execute(message, args) {
+    if ( args[0] == null ) {
+      const newEmbed = new Discord.MessageEmbed()
+          .setTitle('Current Template')
+          .addField(
+              'The current link template is:',
+              config.template)
+          .setColor(config.color);
+      message.channel.send(newEmbed);
+      return;
+    }
     config.template = args[0];
     fs.writeFile(
         path.join(__dirname+'/../config.json'),
